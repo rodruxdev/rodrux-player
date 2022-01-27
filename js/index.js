9541987934b19bd5ed298e4cca84545860e44adc
rodrigojgq <rodrigojgq@gmail.com>
2022-01-27 16:22:57 -0400

    feat:dizzy:: Finish animation in song progress

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
 const likeButton = document.getElementById("like");
 const likeLogo = document.getElementById("like-logo");
+const pausePlayButton = document.getElementById("play-pause");
+const progressBar = document.getElementById("progress-bar");
 
-const likeSong = () => {
-  if(likeLogo.className === "button__logo") {
-    likeLogo.classList.remove("button__logo")
+const togleLikeSong = () => {
+  if(!likeLogo.classList.contains("button__logo--liked")) {
     likeLogo.classList.add("button__logo--liked")
     likeLogo.src = "assets/icons/heart-fill.svg"
   } else {
     likeLogo.classList.remove("button__logo--liked")
-    likeLogo.classList.add("button__logo")
     likeLogo.src = "assets/icons/heart-outline.svg"
-
   }
 }
 
-likeButton.addEventListener('click', likeSong);
+const pausePlaySong = () => {
+  progressBar.classList.toggle("player__progress-bar--pause")
+  if(!progressBar.classList.contains("player__progress-bar--pause")){
+    pausePlayButton.getElementsByTagName('img')[0]
+    .setAttribute("src", "./assets/icons/pause-line.svg");
+  } else {
+    pausePlayButton.getElementsByTagName('img')[0]
+    .setAttribute("src", "./assets/icons/play.svg");
+  }
+}
+
+likeButton.addEventListener('click', togleLikeSong);
+pausePlayButton.addEventListener('click', pausePlaySong);
